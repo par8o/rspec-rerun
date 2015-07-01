9abda06af1c1aaeeb40b16bdd8d393145e6e9e59
Kendal Miller <thekendalmiller@gmail.com>
2015-07-01 11:12:09 -0400

    fix rspec 3.1

--- lib/rspec-rerun/formatter.rb
+++ lib/rspec-rerun/formatter.rb
@@ -11,13 +11,23 @@ module RSpec
         if failed_examples.empty?
           clean!
         else
-          rerun_commands = failed_examples.map(&:rerun_argument)
+          rerun_commands = failed_examples.map { |e| retry_command(e) }
           File.write(FILENAME, rerun_commands.join("\n"))
         end
       end
 
       def clean!
         File.delete FILENAME if File.exist? FILENAME
+      end
+
+      private
+
+      def retry_command(example)
+        if example.respond_to?(:rerun_argument)
+          example.rerun_argument
+        else
+          example.location.gsub("\"", "\\\"")
+        end
       end
     end
   end
